9d365d5e90ad6824533604ba5372ef3647e4e0e0
Anjie Liu <anjieliu@cs.utexas.edu>
2024-02-21 10:58:54 -0600

    about page update tools (done, for now)

--- webpage/static/constants.py
+++ webpage/static/constants.py
@@ -12,7 +12,7 @@ names = {
 # TODO: update your bio please
 member_info = {
     'Anjie Liu': {
-        'image': "https://img.freepik.com/free-photo/cute-domestic-kitten-sits-window-staring-outside-generative-ai_188544-12519.jpg",
+        'image': "../static/images/Anjie.jpeg",
         'bio': 'I am a sophomore Statistics and Data Science student at the University of Texas at Austin. I enjoy reading and exploring different things.',
         'responsibilities': 'tbd',
         'unittests': 0 # unittets will be updated manually
@@ -37,3 +37,23 @@ member_info = {
     }
 }
 
+# TODO: update data sources
+data_source = {
+
+}
+
+tools = {
+    "Bootstrap": {
+        'image': '../static/images/bootstrap.png',
+        'link': 'https://getbootstrap.com'
+    },
+    "GitLab": {
+        'image': '../static/images/gitlab.png',
+        'link': 'https://gitlab.com'
+    },
+    "Google Cloud": {
+        'image': '../static/images/google_cloud.png',
+        'link': 'https://console.cloud.google.com/'
+    }
+
+}
