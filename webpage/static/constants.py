caadbb6c993d17b5e38311e539ba53bd0d36409c
Anjie Liu <anjieliu@cs.utexas.edu>
2024-02-21 20:26:40 -0600

    responsibility, data source, tools update

--- webpage/static/constants.py
+++ webpage/static/constants.py
@@ -14,25 +14,25 @@ member_info = {
     'Anjie Liu': {
         'image': "../static/images/Anjie.jpeg",
         'bio': 'I am a sophomore Statistics and Data Science student at the University of Texas at Austin. I enjoy reading and exploring different things.',
-        'responsibilities': 'tbd',
+        'responsibilities': 'front-end, GitLab API',
         'unittests': 0 # unittets will be updated manually
     },
     'Carson Mullen': {
         'image': "../static/images/CarsonImage.jpg",
         'bio': 'I am a third-year Business Analytics student at UT and am pursuing a career in Data Science.',
-        'responsibilities': 'tbd',
+        'responsibilities': 'front-end, Amadeus API',
         'unittests': 0
     },
     'Kenny Tian': {
         'image': "../static/images/KennyImage.jpg",
         'bio': 'I am a senior biomedical engineering student at The University of Texas at Austin focusing on computiational biomedical engineering.',
-        'responsibilities': 'tbd',
+        'responsibilities': 'front-end, Amadeus API',
         'unittests': 0
     },
     'Kibam Kang': {
         'image': "../static/images/KibamImage.jpeg",
         'bio': 'I am senior student at The University of Texas at Austin. I am a Mathmatician.',
-        'responsibilities': 'tbd',
+        'responsibilities': 'front-end, Amadeus API',
         'unittests': 0
     }
 }
@@ -40,6 +40,14 @@ member_info = {
 # TODO: update data sources
 data_source = {
     "Amadeus Flight Offers Search": {
+        'link': 'https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search',
+        'description': 'We utilized the Amadeus Flight Offers Search API to gather flight data from AUS to 3 popular tourist sites (Berlin, Paris, and NYC) on 2/17. As we add interactiveness to the project, we will show all flights between AUS and the selected cities for the current day.'
+    },
+    "API 2": {
+        'link': 'https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search',
+        'description': 'We utilized the Amadeus Flight Offers Search API to gather flight data from AUS to 3 popular tourist sites (Berlin, Paris, and NYC) on 2/17. As we add interactiveness to the project, we will show all flights between AUS and the selected cities for the current day.'
+    },
+    "API 3": {
         'link': 'https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search',
         'description': 'We utilized the Amadeus Flight Offers Search API to gather flight data from AUS to 3 popular tourist sites (Berlin, Paris, and NYC) on 2/17. As we add interactiveness to the project, we will show all flights between AUS and the selected cities for the current day.'
     },
@@ -61,6 +69,9 @@ tools = {
     "Google Cloud": {
         'image': '../static/images/google_cloud.png',
         'link': 'https://console.cloud.google.com/'
+    },
+    "Flask":{
+        "image": '../static/images/flask.png',
+        'link': 'https://flask.palletsprojects.com/en/3.0.x/'
     }
-
 }
